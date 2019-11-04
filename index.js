81144fd2604f8b290dfca08db4ebafd4cecfb09b
Brad Wood <woodbr@earlham.edu>
2019-11-04 16:33:35 -0500

    update

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 module.exports = {
-   extend: 'apostrophe-module',
-   label: 'Academics',
-   moogBundle: {
-     modules: ['books','courses','courses-pages', 'departments','departments-pages','divisions','majors','majors-pages','majors-widgets'],
-     directory: 'lib/modules'
-   },
-   construct: function(self, options, callback) {
-
-   }
+	label: 'Academics',
+   alias: 'ec-academics',
+   name: 'ec-academics',
+   extend: 'apostrophe-module',   
+	moogBundle: {
+		modules: ['books','courses','courses-pages', 'departments','departments-pages','divisions','majors','majors-pages','majors-widgets'],
+		directory: 'lib/modules'
+	}
 };
